dbd406c5734c9cbcd90298fb414ec24fbebfb64f
jcfenuchi <juliocesarfenuchi@hotmail.com>
2023-11-26 02:27:12 -0300

    fix code

--- echo_server.py
+++ echo_server.py
@@ -3,29 +3,47 @@
 import socket
 from json import loads
 from threading import Thread
+from threading import Event
 
 
 class hardware_server:
     def __init__(self, host: str = "127.0.0.1", port: int = 65432):
         # Port to listen on (non-privileged ports are > 1023)
-        self.__start_socket(host, port)
         self.__hardwares_info = {}
+        self.__threads = {}
+        self.__start_socket(host, port)
 
-    def __handle_client(self, client_sock, addr):
+    def __handle_client(self,event , client_sock, addr):
+        print(client_sock, addr)
         with client_sock:
             while True:
                 data = client_sock.recv(1024)
                 if not data:
                     break
+                if event.is_set():
+                    break
                 cli_hwr_info = loads(data.decode('utf-8'))
                 [cli_hwr_info[key].update({"ip_client": addr[0]}) for key in cli_hwr_info.keys()]
                 self.__hardwares_info.update(cli_hwr_info)
+                print(self.__hardwares_info)
+            self.__threads.pop(addr)
 
     def __start_socket(self, host, port):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.bind((host, port))
             s.listen()
+            event = Event()
             while True:
-                conn, addr = s.accept()
-                th = Thread(target=self.__handle_client, args=(conn, addr))
-                th.start()
+                try:
+                    conn, addr = s.accept()
+                    self.__threads[addr] = Thread(target=self.__handle_client, args=(event, conn, addr))
+                    self.__threads[addr].start()
+                except KeyboardInterrupt:
+                    event.set()
+                    break
+        print("\nClosing server!")
+
+    def __del__(self):
+        print("Finish!")
+
+hardware_server()
--- sock_client.py
+++ sock_client.py
@@ -8,30 +8,33 @@ HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
 
 class Agent_mon(Monitoring):
-    def __init(self, host: str = "127.0.0.1", port: int = 65432):
+    def __init__(self, host: str = "127.0.0.1", port: int = 65432):
         self.__try_con(host, port)
 
     def __try_con(self, host, port):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            try_time = 0
-            timeout_con = 60
-            while True:
-                try:
-                    s.connect((host, port))
-                except Exception:
-                    try_time += 1
-                    print(f"error try time {try_time}")
-                    sleep(timeout_con)
-                    continue
-                break
-            self.__send_hwr_info(s)
-
-    def __send_hwr_info(self, sock):
+        try_time = 0
         while True:
             try:
-                sock.sendall(json.dumps({socket.gethostname():self.get_system_info()}).encode('utf-8'))
+                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                    while True:
+                        try:
+                            s.connect((host, port))
+                            try_time = 0
+                            while True:
+                                self.__send_hwr_info(s)
+                        except Exception:
+                            break
                 sleep(60)
+                try_time += 1
+                print(f"Try Connect to {host}:{port} - try:{try_time}")
             except KeyboardInterrupt:
                 break
-            except Exception as ex:
-                print(f"error {ex}")
+
+    def __send_hwr_info(self, sock):
+        sock.sendall(json.dumps({socket.gethostname():self.get_system_info()}).encode('utf-8'))
+        sleep(5)
+
+    def __del__(self):
+        print("Finish!")
+
+Agent_mon()
