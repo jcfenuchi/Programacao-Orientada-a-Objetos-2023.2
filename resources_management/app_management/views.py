d90eb3403d97916c57e47ec34add6ff9b365bd85
jcfenuchi <juliocesarfenuchi@hotmail.com>
2023-11-27 12:36:19 -0300

    update code

---
+++ resources_management/app_management/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'usuarios/home.html')
+
+def management(request):
+    return render(request, 'usuarios/home.html')
